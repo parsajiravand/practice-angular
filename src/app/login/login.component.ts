67993fc267535c8b2473f63d7c287daba1e090e1
parsa jiravand <parsajiravand@gmail.com>
2022-07-30 04:01:40 +0430

    set token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../service/core/auth.service';
 import { Router } from '@angular/router';
+//@ts-ignore
+import Cookies from 'js-cookie';
+
+class Response {
+  constructor(
+    public status: boolean,
+    public message: string,
+    public data: object | string[]
+  ) {}
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,8 +20,12 @@ export class LoginComponent {
   loading = false;
   submitLoginForm(payload: object) {
     this.loading = true;
-    this.loginService.login(payload).then((_response) => {
-      console.log('res from comp', _response);
+    this.loginService.login(payload).then((res: Response | any) => {
+      console.log('ressssssss', res.status);
+      if (res.status) {
+        Cookies.set('NG_USER', JSON.stringify(res.data.user), { expires: 60 });
+        this.router.navigate(['/']);
+      }
       this.loading = false;
     });
   }
--- src/app/service/core/auth.service.ts
+++ src/app/service/core/auth.service.ts
@@ -1,33 +1,39 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 class Login {
+  constructor(public email: string, public password: string) {}
+}
+class Response {
   constructor(
-    public email: string,
-    public password: string,
+    public status: boolean,
+    public message: string,
+    public data: object | string[]
   ) {}
 }
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private http: HttpClient,@Inject('BASE_API_URL') private baseUrl: string) {
-    
-  }
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_API_URL') private baseUrl: string
+  ) {}
   login(payload: Object) {
-     const promise = new Promise<void>((resolve, reject) => {
-      this.http.post<Login>(`${this.baseUrl}/api/user/login`,
-        payload).subscribe({
-        next: (res: any) => {
-          console.log(res)
-          resolve();
-        },
-        error: (err: any) => {
-         resolve(err)
-        },
-        complete: () => {
-          console.log('complete');
-        },
-      });
+    const promise = new Promise<void>((resolve, reject) => {
+      this.http
+        .post<Login>(`${this.baseUrl}/api/user/login`, payload)
+        .subscribe({
+          next: (res: any) => {
+            console.log(res);
+            resolve(res);
+          },
+          error: (err: any) => {
+            resolve(err.error);
+          },
+          complete: () => {
+            console.log('complete');
+          },
+        });
     });
     return promise;
   }
